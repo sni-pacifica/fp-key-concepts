87e4c179bb0d7ec52961cc3f2819949a0a1c68e4
sni-pacifica <sebastien.nichele@ca-pacifica.fr>
2020-02-06 16:39:25 +0100

    Added functions implementation code.

--- src/city-functions/Functions.ts
+++ src/city-functions/Functions.ts
@@ -2,24 +2,27 @@ import {Types} from "./Types";
 
 export namespace Pure {
 
-    import City = Types.City;
-    import Position2D = Types.Position2D;
-    import DistanceInMeters = Types.DistanceInMeters;
-    import DistanceInKilometers = Types.DistanceInKilometers;
+  import City = Types.City;
+  import Position2D = Types.Position2D;
+  import DistanceInMeters = Types.DistanceInMeters;
+  import DistanceInKilometers = Types.DistanceInKilometers;
 
-    export const getLocation: (v: City) => Position2D =
-        v => void 0; // TODO !
 
-    // TODO ! Utilisez le théorème de pythagore (namespace Pythagoras ci-dessous),
-    // la distance entre deux points est égale à l'hypothénuse.
-    export const distanceBetween: (l1: Position2D, l2: Position2D) => DistanceInMeters =
-        (l1, l2) => void 0;
+  export const getLocation: (v: City) => Position2D =
+    v => v.position2D;
 
-    export const metersToKilometers: (d: DistanceInMeters) => DistanceInKilometers =
-        d => void 0;// TODO !
+  export const distanceBetween: (l1: Position2D, l2: Position2D) => DistanceInMeters =
+    (l1, l2) => Pythagoras.hypotenuse(
+      l1.x - l2.x,
+      l1.y - l2.y
+    )
 
-    export const distanceMessage: (v1: City, v2: City, distance: DistanceInKilometers) => string =
-        (v1, v2, d) => void 0;// TODO !
+  export const metersToKilometers: (d: DistanceInMeters) => DistanceInKilometers =
+    d => d / 1000;
+
+  export const distanceMessage: (v1: City, v2: City, distance: DistanceInKilometers) => string =
+    (v1, v2, d) => `La distance entre ${v1.name} et ${v2.name} est de ${d} kms`;
+
 
 }
 
